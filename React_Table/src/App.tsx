1f80d85acd5cf88dfcd0363d7f6fbb889feb1a05
Taha Akber <taha.akber007@gmail.com>
2023-03-30 19:32:27 +0500

    Create table using React _JS(Typesript)

---
+++ React_Table/src/App.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  website: string;
+}
+
+function App() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => setUsers(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
