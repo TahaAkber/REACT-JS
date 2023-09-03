5dab8ac9f5a4f379ddd89b330b3c78e0564063f1
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-09-04 01:15:00 +0500

    module 3 completed

--- reactpractice/List, Ternary Op, List, Css/src/App.js
+++ reactpractice/List, Ternary Op, List, Css/src/App.js
@@ -5,30 +5,45 @@ import User from "./Components/User";
 function App() {
   // const isGreen = true;
   // const Age = 19;
-  const names = ["Taha", "Akber", "Zulminun", "Talha", "Abdullah"];
-  const Users = [
-    { name: "Taha", age: 22 },
-    { name: "Talha", age: 24 },
-    { name: "Zulminun", age: 21 },
+
+  // mapped this using props and component
+  // const Users = [
+  //   { name: "Taha", age: 22 },
+  //   { name: "Talha", age: 24 },
+  //   { name: "Zulminun", age: 21 },
+  // ];
+  //Exercise
+  const planets = [
+    { name: "Mars", isGasPlanet: false },
+    { name: "Earth", isGasPlanet: false },
+    { name: "Jupiter", isGasPlanet: true },
+    { name: "Venus", isGasPlanet: false },
+    { name: "Neptune", isGasPlanet: true },
+    { name: "Uranus", isGasPlanet: true },
   ];
 
   return (
     <div className="App">
+      {/* Map using props and component dynamic */}
+      {/* {Users.map((user) => {
+        return (
+          <div>
+            <User name={user.name} age={user.age} />
+          </div>
+        );
+      })} */}
       {/* Ternary Operators if and else.. */}
       {/* {Age >= 18 ? <h3>Over Age</h3> : <h3>Under Age</h3>}
       <h1 style={{ color: isGreen ? "green" : "red" }}>This is the Color</h1> */}
       {/* This is if statement */}
       {/* {isGreen && <button> This is button</button>} */}
-
       {/* simple user nhi lagega kuyun k Users wale function ko object banadia hai tou dot krke access hongi cheezein */}
-
       {/* props */}
       {/* <Job salary={25000} position="Senior Dev" company="Amazon" />
       <Job salary={20000} position="Junior Dev" company="Apple" />
       <Job salary={10000} position="Project Manager" company="Google" /> */}
 
-
-      
+      {planets.map((i) => i.isGasPlanet && <h2>{i.name}</h2>)}
     </div>
   );
 }
---
+++ reactpractice/List, Ternary Op, List, Css/src/Components/Planet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Planet(props) {
+  return (
+      <div>
+          {props.name}
+    </div>
+  )
+}
+
+export default Planet
