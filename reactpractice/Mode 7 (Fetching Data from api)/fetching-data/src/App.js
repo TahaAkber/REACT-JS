b85b72b4b77c5b82d58ec1a0c6198156ad13ae07
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-09-17 18:22:19 +0500

    Update App.js

--- reactpractice/Mode 7 (Fetching Data from api)/fetching-data/src/App.js
+++ reactpractice/Mode 7 (Fetching Data from api)/fetching-data/src/App.js
@@ -2,12 +2,17 @@ import "./App.css";
 import Axios from "axios";
 import { useEffect, useState } from "react";
 function App() {
-  Axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
-    console.log(res.data);
-  });
+  const [data, setdata] = useState("");
+  useEffect(() => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setdata(res.data.fact);
+    });
+  }, []);
+
   return (
     <div className="App">
       <button>Generate Table</button>
+      <h2>{data}</h2>
     </div>
   );
 }
