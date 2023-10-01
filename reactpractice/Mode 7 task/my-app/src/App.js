3bd17f81590ef0d0c2209818060117f89e17714d
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-10-01 17:33:36 +0500

    task completed

--- reactpractice/Mode 7 task/my-app/src/App.js
+++ reactpractice/Mode 7 task/my-app/src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
+  const [excuse, setexcuse] = useState("");
+  const fetchdata = (excuse) => {
+    axios
+      .get(`https://excuser-three.vercel.app/v1/excuse/${excuse}`)
+      .then((res) => {
+        setexcuse(res.data[0].excuse);
+        console.log(res.data);
+      });
+  };
   return (
     <div className="App">
-      
+      <h3>Generate an Excuse</h3>
+      <button onClick={() => fetchdata("family")}>Family Excuse</button>
+      <button onClick={() => fetchdata("party")}>Party</button>
+      <button onClick={() => fetchdata("office")}>Office</button>
+      <div>
+        <p>{excuse}</p>
+      </div>
     </div>
   );
 }
