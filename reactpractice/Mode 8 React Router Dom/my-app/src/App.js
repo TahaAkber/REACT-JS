92b5a9d9d87d01632c762a41adf31ede13cadd52
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-10-05 23:48:26 +0500

    few more pages

--- reactpractice/Mode 8 React Router Dom/my-app/src/App.js
+++ reactpractice/Mode 8 React Router Dom/my-app/src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Menu from "./Pages/Menu";
+import Contact from "./Pages/Contact";
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
