b01dba5b5c9dd3abcedec93a067b319f457512cc
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-10-13 23:06:46 +0500

    Navbar

--- reactpractice/Mode 8 React Router Dom/my-app/src/App.js
+++ reactpractice/Mode 8 React Router Dom/my-app/src/App.js
@@ -3,17 +3,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Pages/Home";
 import Menu from "./Pages/Menu";
 import Contact from "./Pages/Contact";
+import Navbar from "./Pages/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          NAVBAR
-          <Link to={"/"}> Home</Link>
-          <Link to={"/Menu"}> Menu</Link>
-          <Link to={"/contact"}> contact</Link>
-        </div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Menu" element={<Menu />} />
