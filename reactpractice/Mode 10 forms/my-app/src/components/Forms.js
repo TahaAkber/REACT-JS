0ccb798532ce7b025b600f5bb9dd48314a19618c
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-12-11 02:41:43 +0500

    Completed validation

--- reactpractice/Mode 10 forms/my-app/src/components/Forms.js
+++ reactpractice/Mode 10 forms/my-app/src/components/Forms.js
@@ -9,9 +9,15 @@ const Forms = () => {
     email: yup.string().email().required(),
     age: yup.number().positive().integer().min(18).required(),
     password: yup.string().min(4).max(20).required(),
-    confirmPassword: yup.string().oneOf([yup.ref("password"), null]),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), null], "Passwords not match"),
   });
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
   });
   const onSubmit = (data) => {
@@ -20,18 +26,27 @@ const Forms = () => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input type="text" placeholder="fullname..." {...register("fullName")} />
+      <p id="paragraph">{errors.fullName?.message}</p>
       <input type="text" placeholder="Email..." {...register("email")} />
+      <p id="paragraph">{errors.email?.message}</p>
+
       <input type="number" placeholder="Age..." {...register("age")} />
+      <p id="paragraph">{errors.age?.message}</p>
+
       <input
         type="password"
         placeholder="Password..."
         {...register("password")}
       />
+      <p id="paragraph">{errors.password?.message}</p>
+
       <input
         type="password"
         placeholder="confirm password..."
         {...register("confirmPassword")}
       />
+      <p id="paragraph">{errors.confirmPassword?.message}</p>
+
       <input type="submit" />
     </form>
   );
