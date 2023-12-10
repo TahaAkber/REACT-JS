d369ce2ff3f171483b71436eed7abad491f30823
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-12-11 02:22:15 +0500

    new mod

---
+++ reactpractice/Mode 10 forms/my-app/src/components/Forms.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const Forms = () => {
+  //   const schema = yup.object().shape({
+  //     fullName: yup.string().required(),
+  //     email: yup.string().email().required(),
+  //     age: yup.number().positive().integer().min(18).required(),
+  //     password: yup.string().number().min(4).max(10).required(),
+  //     confirmPassword: yup.string().oneOf([yup.ref("Password"), null]),
+  //   });
+  const { register, handleSubmit } = useForm({
+    //resolver: yupResolver(schema),
+  });
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="fullname..." {...register("fullName")} />
+      <input type="text" placeholder="Email..." {...register("email")} />
+      <input type="number" placeholder="Age..." {...register("age")} />
+      <input
+        type="password"
+        placeholder="Password..."
+        {...register("password")}
+      />
+      <input
+        type="password"
+        placeholder="confirm password..."
+        {...register("confirmPassword")}
+      />
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default Forms;
