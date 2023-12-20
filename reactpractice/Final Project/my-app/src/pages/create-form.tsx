29682cd62b88a3f261ef888e2d3e7f1b98bd9584
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-12-21 00:18:59 +0500

    Update Create-form.tsx validation and typescript

--- reactpractice/Final Project/my-app/src/pages/create-form.tsx
+++ reactpractice/Final Project/my-app/src/pages/create-form.tsx
@@ -1,10 +1,35 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface CreateFormdata {
+  title: string;
+  description: string;
+}
 export const CreateForm = () => {
+  const schema = yup.object().shape({
+    title: yup.string().required("You must add an Title"),
+    description: yup.string().required("You must add an Description"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateFormdata>({
+    resolver: yupResolver(schema),
+  });
+  //any pe kaam horha hai but yahan typescript lagayenge aur data k interface banayenge
+
+  const onCreatepost = (data: CreateFormdata) => {
+    console.log(data);
+  };
   return (
-    <form>
-      <input placeholder="Tile..."></input>
-      <input placeholder="Description..." />
+    <form onSubmit={handleSubmit(onCreatepost)}>
+      <input placeholder="Tile..." {...register("title")}></input>
+      <p style={{ color: "red" }}>{errors.title?.message}</p>
+      <textarea placeholder="Description..." {...register("description")} />
+      <p style={{ color: "red" }}>{errors.description?.message}</p>
+
       <input type="submit"></input>
     </form>
   );
