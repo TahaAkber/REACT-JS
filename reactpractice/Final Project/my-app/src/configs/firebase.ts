488e2520ef9b537ed4c724d6b79846b55b54b379
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-12-22 15:26:30 +0500

    Update firebase and connect our front end to database

--- reactpractice/Final Project/my-app/src/configs/firebase.ts
+++ reactpractice/Final Project/my-app/src/configs/firebase.ts
@@ -1,7 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {GoogleAuthProvider,getAuth} from 'firebase/auth';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
+import { getFirestore} from 'firebase/firestore';
+//grab data and send to database from out app
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +27,4 @@ export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app); 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
--- reactpractice/Final Project/my-app/src/pages/create-form.tsx
+++ reactpractice/Final Project/my-app/src/pages/create-form.tsx
@@ -1,12 +1,18 @@
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
-
+//yeh dono niche wale add kiye hain kyun k data base me entry k liye 2 cheezein chahiye hoti hain aik colleciton k kahan data add horha
+//second document k us mein document kiya hai...
+import { collection, addDoc } from "firebase/firestore";
+import { db, auth } from "../configs/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 interface CreateFormdata {
   title: string;
   description: string;
 }
 export const CreateForm = () => {
+  const [user] = useAuthState(auth);
+
   const schema = yup.object().shape({
     title: yup.string().required("You must add an Title"),
     description: yup.string().required("You must add an Description"),
@@ -18,10 +24,17 @@ export const CreateForm = () => {
   } = useForm<CreateFormdata>({
     resolver: yupResolver(schema),
   });
-  //any pe kaam horha hai but yahan typescript lagayenge aur data k interface banayenge
+  //db database, posts hamari database me entry ki hai
+  const postsref = collection(db, "posts");
 
-  const onCreatepost = (data: CreateFormdata) => {
-    console.log(data);
+  //any pe kaam horha hai but yahan typescript lagayenge aur data k interface banayenge
+  const onCreatepost = async (data: CreateFormdata) => {
+    await addDoc(postsref, {
+      title: data.title,
+      description: data.description,
+      username: user?.displayName,
+      userId: user?.uid,
+    });
   };
   return (
     <form onSubmit={handleSubmit(onCreatepost)}>
