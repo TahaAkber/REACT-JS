9a0b443d5544494c9e85dfcdc94eafb09fd07902
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-12-24 17:14:02 +0500

    Update post.tsx (hardpart)

--- reactpractice/Final Project/my-app/src/pages/main/post.tsx
+++ reactpractice/Final Project/my-app/src/pages/main/post.tsx
@@ -1,10 +1,29 @@
+import { useEffect } from "react";
+import { db, auth } from "../../configs/firebase";
 import { post as Ipost } from "./main";
+import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 interface props {
   post: Ipost;
 }
 export const Post = (props: props) => {
-  //structure data of post as props
   const { post } = props;
+  const [user] = useAuthState(auth);
+  const likesref = collection(db, "likes");
+  const getlikes = async () => {
+    const data = await getDocs(likesDoc);
+    console.log(data.docs.map((i) => ({ ...i.data(), id: i.id })));
+  };
+  const likesDoc = query(likesref, where("postId", "==", post.id));
+  const Addlike = async () => {
+    await addDoc(likesref, { userId: user?.uid, postId: post.id });
+  };
+  useEffect(() => {
+    getlikes();
+  }, []);
+
+  //structure data of post as props
   return (
     <div>
       <div className="title">
@@ -13,8 +32,9 @@ export const Post = (props: props) => {
       <div className="description">
         <p>{post.description}</p>
       </div>
-      <div>
+      <div className="footer">
         <p>@{post.username}</p>
+        <button onClick={Addlike}>&#128077;</button>
       </div>
     </div>
   );
