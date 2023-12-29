ff7953c8b0e31b5a8938f6c19752488e4251adb3
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-12-29 23:58:55 +0500

    form

--- reactpractice/Testsproject/my-app/src/pages/createform.tsx
+++ reactpractice/Testsproject/my-app/src/pages/createform.tsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 function Createform() {
-  return <form></form>;
+  return (
+    <form>
+      <input placeholder="title"></input>
+      <textarea placeholder="description"></textarea>
+      <input type="submit"></input>
+    </form>
+  );
 }
 
 export default Createform;
