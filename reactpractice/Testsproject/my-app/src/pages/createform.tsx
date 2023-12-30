f20f2a50239ce08ca54cd99089c51bb550e278d8
Taha <123082592+TahaAkber@users.noreply.github.com>
2023-12-30 20:24:00 +0500

    database pe data daldia

--- reactpractice/Testsproject/my-app/src/pages/createform.tsx
+++ reactpractice/Testsproject/my-app/src/pages/createform.tsx
@@ -1,12 +1,46 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
+import { addDoc, collection } from "firebase/firestore";
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { db, auth } from "../configs/firebase";
+interface createform {
+  title: string;
+  description: string;
+}
 function Createform() {
+  const schema = yup.object().shape({
+    title: yup.string().required("Title must not be empty"),
+    description: yup.string().required("Description must be entered"),
+  });
+  const [user] = useAuthState(auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<createform>({
+    resolver: yupResolver(schema),
+  });
+  const postref = collection(db, "posts");
+  const createpost = async (data: createform) => {
+    await addDoc(postref, {
+      title: data.title,
+      description: data.description,
+      username: user?.displayName,
+      userId: user?.uid,
+    });
+  };
   return (
-    <form>
-      <input placeholder="title"></input>
-      <textarea placeholder="description"></textarea>
+    <form onSubmit={handleSubmit(createpost)}>
+      <input placeholder="title" {...register("title")}></input>
+      <p style={{ color: "red" }}>{errors.title?.message}</p>
+      <textarea
+        placeholder="description"
+        {...register("description")}
+      ></textarea>
+      <p style={{ color: "red" }}>{errors.description?.message}</p>
       <input type="submit"></input>
     </form>
   );
