e64c8285657f2c639fd3e4a6804466037a60ccac
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-20 01:56:19 +0500

    Navbar css

--- reactpractice/page-transition-react/src/components/navbar/Navbar.jsx
+++ reactpractice/page-transition-react/src/components/navbar/Navbar.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div className="parent-c1">
-      <Link to="/">Home</Link>
-      <Link to="/Menu">Menu</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
+    <div className="parent-n1">
+      <div className="child-n1">
+        <div className="div1">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="div1">
+          <Link to="/Menu">Menu</Link>
+        </div>
+        <div className="div1">
+          <Link to="/about">About</Link>
+        </div>
+        <div className="div1">
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
     </div>
   );
 }
