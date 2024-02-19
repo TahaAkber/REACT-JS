6b350a38623007c24f8f02b08327cb3d21122140
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-20 03:06:58 +0500

    adding some functionality

--- reactpractice/page-transition-react/src/components/navbar/Navbar.jsx
+++ reactpractice/page-transition-react/src/components/navbar/Navbar.jsx
@@ -5,16 +5,24 @@ function Navbar() {
     <div className="parent-n1">
       <div className="child-n1">
         <div className="div1">
-          <Link to="/">Home</Link>
+          <Link to="/" style={{ color: "red" }}>
+            Home
+          </Link>
         </div>
         <div className="div1">
-          <Link to="/Menu">Menu</Link>
+          <Link to="/Menu" style={{ color: "red" }}>
+            Menu
+          </Link>
         </div>
         <div className="div1">
-          <Link to="/about">About</Link>
+          <Link to="/about" style={{ color: "red" }}>
+            About
+          </Link>
         </div>
         <div className="div1">
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact" style={{ color: "red" }}>
+            Contact
+          </Link>
         </div>
       </div>
     </div>
--- reactpractice/page-transition-react/src/components/pages/Menu.jsx
+++ reactpractice/page-transition-react/src/components/pages/Menu.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import axios from "axios";
+import "../style/menu.css";
 function Menu() {
-  return <div>Menu</div>;
+  return (
+    <div className="menu-p1">
+      <div className="menu-parent-p1">
+        <div className="menu-child-p1">asd</div>
+        <div className="menu-child-p1">asd</div>
+        <div className="menu-child-p1">asd</div>
+        <div className="menu-child-p1">asd</div>
+        <div className="menu-child-p1">asd</div>
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
