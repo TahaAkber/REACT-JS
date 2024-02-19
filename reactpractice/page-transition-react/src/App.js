5a745f6c94b90973cc56754a941155f81f0e5e0a
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-20 00:41:27 +0500

    doing css in navigation bar

--- reactpractice/page-transition-react/src/App.js
+++ reactpractice/page-transition-react/src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Menu from "./components/pages/Menu";
+import Navbar from "./components/navbar/Navbar";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/Menu" element={<Menu />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
