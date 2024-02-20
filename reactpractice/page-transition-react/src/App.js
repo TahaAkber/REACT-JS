1a25bf7661ac94168754dd8383579ac8ae93672b
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-20 20:04:32 +0500

    key and location added
    framer-motion library installed then we import uselocation from react-router-dom so that we can know which page we are inside of, then import animatepresence from framer-motion

--- reactpractice/page-transition-react/src/App.js
+++ reactpractice/page-transition-react/src/App.js
@@ -1,22 +1,31 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Menu from "./components/pages/Menu";
 import Navbar from "./components/navbar/Navbar";
+import { AnimatePresence } from "framer-motion";
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/Menu" element={<Menu />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-        </Routes>
+        <AnimatePresence>
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/Menu" element={<Menu />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+        </AnimatePresence>
       </Router>
     </div>
   );
--- reactpractice/page-transition-react/src/components/pages/Menu.jsx
+++ reactpractice/page-transition-react/src/components/pages/Menu.jsx
@@ -2,15 +2,12 @@ import React from "react";
 import axios from "axios";
 import "../style/menu.css";
 function Menu() {
+  const getdata = () => {
+    axios.get("")
+  }
   return (
     <div className="menu-p1">
-      <div className="menu-parent-p1">
-        <div className="menu-child-p1">asd</div>
-        <div className="menu-child-p1">asd</div>
-        <div className="menu-child-p1">asd</div>
-        <div className="menu-child-p1">asd</div>
-        <div className="menu-child-p1">asd</div>
-      </div>
+      <div className="menu-parent-p1"></div>
     </div>
   );
 }
