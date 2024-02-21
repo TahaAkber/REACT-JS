7ad716b0f5562e6217a0c025faa33acf296a3749
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-21 19:52:16 +0500

    home page and css added

--- reactpractice/page-transition-react/src/components/pages/Home.jsx
+++ reactpractice/page-transition-react/src/components/pages/Home.jsx
@@ -8,7 +8,11 @@ function Home() {
       animate={{ width: "100%" }}
       exit={{ x: window.innerWidth, transition: { duration: 0.2 } }}
     >
-      Home
+      <div className="home-p1">
+        <div className="header-image">
+          <h2>Welcome to Cars Show Room</h2>
+        </div>
+      </div>
     </motion.div>
   );
 }
