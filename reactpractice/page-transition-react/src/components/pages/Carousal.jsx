da75b6684f2574778733e3fcfe8c8a09f0c11e62
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-26 18:12:49 +0500

    carousal added

--- reactpractice/page-transition-react/src/components/pages/Carousal.jsx
+++ reactpractice/page-transition-react/src/components/pages/Carousal.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Carousal.css";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 const slidedata = {
@@ -17,16 +17,26 @@ const slidedata = {
     },
   ],
 };
-
 function Carousal() {
-  console.log(slidedata);
+  const [slide, setslide] = useState(0);
+  const functionright = () => {
+    setslide(slide === slidedata.slides.length - 1 ? 0 : slide + 1);
+  };
+  const functionleft = () => {
+    setslide(slide === 0 ? slidedata.slides.length - 1 : slide - 1);
+  };
   return (
     <div className="parent-src">
-      <BsArrowLeftCircleFill className="arrow-left" />
-      {slidedata.slides.map((i) => (
-        <img src={i.src} alt={i.alt} className="slides" />
+      <BsArrowLeftCircleFill className="arrow-left" onClick={functionleft} />
+      {slidedata.slides.map((i, idx) => (
+        <img
+          src={i.src}
+          alt={i.alt}
+          key={idx}
+          className={slide === idx ? "slides" : "slides slide-hidden"}
+        />
       ))}
-      <BsArrowRightCircleFill className="arrow-right" />
+      <BsArrowRightCircleFill className="arrow-right" onClick={functionright} />
       <span></span>
     </div>
   );
