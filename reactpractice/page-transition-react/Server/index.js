03c1568fea4f2127a4909afcd9af1c2fb078e97b
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-26 19:51:02 +0500

    website finally completed with comments connected to database

--- reactpractice/page-transition-react/Server/index.js
+++ reactpractice/page-transition-react/Server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const Carmodel = require("./models/Carmodel");
-
+const Commentmodel = require("./models/Queries");
 app.use(express.json());
 app.use(cors());
 
@@ -27,6 +27,25 @@ app.post("/car", async (req, res) => {
     res.json(result);
   } catch (error) {
     console.log(error, "Cant add to database");
+  }
+});
+
+app.post("/query", async (req, res) => {
+  try {
+    const formdata = req.body;
+    const result = new Commentmodel(formdata);
+    await result.save();
+    res.json(result);
+  } catch (error) {
+    console.log(error, "Cant accept query");
+  }
+});
+app.get("/query", async (req, res) => {
+  try {
+    const data = await Commentmodel.find({});
+    res.json(data);
+  } catch (error) {
+    console.log("Error data is not inserted: ", error);
   }
 });
 app.listen(3001, () => {
--- reactpractice/page-transition-react/src/components/pages/Contact.jsx
+++ reactpractice/page-transition-react/src/components/pages/Contact.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { motion } from "framer-motion";
 import Carousal from "./Carousal";
-import { slides } from "/Users/KOLN1CE/Documents/GitHub/REACT-JS/reactpractice/page-transition-react/src/imagedata/Data.json";
+import axios from "axios";
 import * as yup from "yup";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -18,9 +17,18 @@ function Contact() {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    await axios
+      .post("http://localhost:3001/query", {
+        name: data.Name,
+        email: data.Email,
+        query: data.Query,
+      })
+      .then((res) => {
+        console.log(data);
+      });
   };
+
   return (
     <motion.div
       initial={{ width: 0 }}
@@ -34,11 +42,11 @@ function Contact() {
         </div>
       </div>
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder="Name" type="text" {...register("fullname")} />
+        <input placeholder="Name" type="text" {...register("Name")} />
         <p style={{ color: " red" }}>{errors.Name?.message}</p>
-        <input placeholder="Email" type="email" {...register("email")} />
+        <input placeholder="Email" type="email" {...register("Email")} />
         <p style={{ color: "red" }}>{errors.Email?.message}</p>
-        <textarea placeholder="Query" type="text" {...register("query")} />
+        <textarea placeholder=" Query" {...register("Query")} />
         <p style={{ color: "red" }}>{errors.Query?.message}</p>
         <input type="submit" />
       </form>
