8196945c94f5c275eda26133cf4287644d204ac6
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-26 19:19:29 +0500

    added validation

--- reactpractice/page-transition-react/src/components/pages/Contact.jsx
+++ reactpractice/page-transition-react/src/components/pages/Contact.jsx
@@ -3,12 +3,13 @@ import { motion } from "framer-motion";
 import Carousal from "./Carousal";
 import { slides } from "/Users/KOLN1CE/Documents/GitHub/REACT-JS/reactpractice/page-transition-react/src/imagedata/Data.json";
 import * as yup from "yup";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 function Contact() {
-  const schema = yup.object.shape({
+  const schema = yup.object().shape({
     Name: yup.string().required(),
     Email: yup.string().email().required(),
-    password: yup.string().min(4).max(200).required(),
+    Query: yup.string().min(4).max(200).required(),
   });
   const {
     register,
@@ -17,7 +18,7 @@ function Contact() {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const onsubmit = (data) => {
+  const onSubmit = (data) => {
     console.log(data);
   };
   return (
@@ -32,10 +33,14 @@ function Contact() {
           <Carousal />
         </div>
       </div>
-      <form className="form" onsubmit={handleSubmit(onsubmit)}>
+      <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <input placeholder="Name" type="text" {...register("fullname")} />
+        <p style={{ color: " red" }}>{errors.Name?.message}</p>
         <input placeholder="Email" type="email" {...register("email")} />
+        <p style={{ color: "red" }}>{errors.Email?.message}</p>
         <textarea placeholder="Query" type="text" {...register("query")} />
+        <p style={{ color: "red" }}>{errors.Query?.message}</p>
+        <input type="submit" />
       </form>
     </motion.div>
   );
