2800a7b3f33972a93f4f5cc3dff7299158240f92
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-26 16:23:28 +0500

    adding contact info

--- reactpractice/page-transition-react/src/components/pages/Contact.jsx
+++ reactpractice/page-transition-react/src/components/pages/Contact.jsx
@@ -8,7 +8,16 @@ function Contact() {
       animate={{ width: "100%" }}
       exit={{ x: window.innerWidth, transition: { duration: 0.2 } }}
     >
-      <div className="contact-p1"></div>
+      <div className="contact-p1">
+        <div className="image">
+          <img
+            src="https://images.unsplash.com/photo-1629019878500-302b516de84c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="image"
+            width={800}
+            height={800}
+          />
+        </div>
+      </div>
     </motion.div>
   );
 }
