26217e43714412c6b14b2a0d9166049222bff92e
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-29 23:18:22 +0500

    search bar and search result container

--- reactpractice/searchbar/src/App.js
+++ reactpractice/searchbar/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="searchbarcontainer">
+        <div className="searchbar"></div>
+        <div className="searchresults"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
