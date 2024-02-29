807d765e5e5ae06b2a7492d33f3a07d05962d785
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-02-29 23:44:42 +0500

    components added

--- reactpractice/searchbar/src/App.js
+++ reactpractice/searchbar/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import Searchbar from "./components/Searchbar";
 
 function App() {
   return (
     <div className="App">
       <div className="searchbarcontainer">
-        <div className="searchbar"></div>
+        <div className="searchbar">
+          <Searchbar />
+        </div>
         <div className="searchresults"></div>
       </div>
     </div>
