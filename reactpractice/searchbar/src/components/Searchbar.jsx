aec6f7484be842a9b9687d42e65e90efc684b859
Taha <123082592+TahaAkber@users.noreply.github.com>
2024-03-02 22:05:02 +0500

    fetched api

--- reactpractice/searchbar/src/components/Searchbar.jsx
+++ reactpractice/searchbar/src/components/Searchbar.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Searchbar.css";
+import Axios from "axios";
 import { FaSearch } from "react-icons/fa";
 function Searchbar() {
+  const [input, setinput] = useState("");
+  const getData = () => {
+    Axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  const handleonclick = (value) => {
+    setinput(value);
+    getData(value);
+  };
   return (
     <div className="input-search">
       <div className="input-body">
         <FaSearch id="search-icon" />
-        <input placeholder="Type to Search..." />
+        <input
+          placeholder="Type to Search..."
+          value={input}
+          onChange={(e) => {
+            handleonclick(e.target.value);
+          }}
+        />
       </div>
     </div>
   );
